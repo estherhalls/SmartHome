e787538325e52e1956a46bc5084aac88d6591b16
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-09-28 15:23:10 -0600

    set up model controller

--- SmartHome/Controllers/Model Controllers/DeviceController.swift
+++ SmartHome/Controllers/Model Controllers/DeviceController.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+class DeviceController {
+    
+    // MARK: - Properties
+    // Singleton
+    static let sharedInstance = DeviceController()
+    
+    // SOT
+    private (set) var devices: [Device] = []
+    
+    // Class Initialization
+    init() {
+        loadDevices()
+    }
+    
+    // Parameters and Methods
+    func create() {
+        saveDevices()
+    }
+    
+    func delete() {
+        saveDevices()
+    }
+    
+    func toggleIsOn(device: Device) {
+        device.isOn.toggle()
+        saveDevices()
+    }
+    
+    // Persistance
+    func saveDevices() {
+        
+    }
+    func loadDevices() {
+        
+    }
+} // End of Class
--- SmartHome/Models/Device.swift
+++ SmartHome/Models/Device.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+class Device: Codable {
+    var name: String
+    var isOn: Bool
+    let id: UUID
+    
+    init(name: String, isOn: Bool = false, id: UUID = UUID()){
+        self.name = name
+        self.isOn = isOn
+        self.id = id
+    }
+} // End of Class
+
+extension Device: Equatable {
+    static func == (lhs: Device, rhs: Device) -> Bool {
+        return lhs.id == rhs.id
+    }
+} // End of Extension
