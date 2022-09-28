2750e1752bce463fc3ac83d80776bf6ededc5b66
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-09-28 16:27:32 -0600

    Created Alert Controller

--- SmartHome/Controllers/Custom Cell/DeviceTableViewCell.swift
+++ SmartHome/Controllers/Custom Cell/DeviceTableViewCell.swift
@@ -6,18 +6,28 @@
 //
 
 import UIKit
+protocol DeviceTableViewCellDelegate: AnyObject {
+    func deviceToggleTapped(cell: DeviceTableViewCell)
+}
 
 class DeviceTableViewCell: UITableViewCell {
     
     // MARK: - Outlets
-    
     @IBOutlet weak var deviceNameLabel: UILabel!
     @IBOutlet weak var switchToggle: UISwitch!
     
+    // MARK: - Properties
+    weak var delegate: DeviceTableViewCellDelegate?
     
+    // MARK: - Methods
+    func updateViews(device: Device) {
+        deviceNameLabel.text = device.name
+        switchToggle.isOn = device.isOn
+    }
     
     // MARK: - Actions
     @IBAction func switchToggleTapped(_ sender: Any) {
+        delegate?.deviceToggleTapped(cell: self)
     }
     
 } // End of Class
