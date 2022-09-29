a96401daf3d0497351c884597625d3c4bef3f5ff
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-09-29 14:18:08 -0600

    Created and subclassed view controller, declared global properties and created notifications within buttons

---
+++ SmartHome/ViewControllers/ToggleAllDevicesViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ToggleAllDevicesViewController.swift
+//  SmartHome
+//
+//  Created by Esther on 9/29/22.
+//
+
+import UIKit
+// Global Property
+
+let turnOnAllNotificationName = Notification.Name(rawValue: "TurnOnAllDevicesNotification")
+let turnOffAllNotificationName = Notification.Name(rawValue: "TurnOffAllDevicesNotification")
+
+class ToggleAllDevicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func turnAllOnButtonTapped(_ sender: UIButton) {
+        NotificationCenter.default.post(name: turnOnAllNotificationName, object: nil)
+    }
+    
+    @IBAction func turnAllOffButtonTapped(_ sender: UIButton) {
+        NotificationCenter.default.post(name: turnOffAllNotificationName, object: nil)
+    }
+}
